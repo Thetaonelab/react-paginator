0b39b5d8145c1915ed5986206074f07a8a83d62a
Debajyoti Bhaumik <code.debjyoti@gmail.com>
2020-02-22 10:26:54 +0530

    merge upstream changes; use wildcard react versions with package-lock.json

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,57 +1,93 @@
-import React, { Component } from 'react'
-import PaginationBody from './PaginationBody'
-
+import React, { Component } from "react";
+import PaginationBody from "./PaginationBody";
 
 class Pagination extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPage: this.props.selectedPage || 1,
-      paginationCount: this.props.count || 6,
+      selectedPage: 1,
+      paginationCount: 6,
       paginationStart: 1,
-      paginationEnd: this.props.totalPages || 21
-
-    }
-    this.handlePageChange = this.handlePageChange.bind(this)
+      paginationEnd: 21
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+  componentDidMount() {
+    const paginationEnd = Math.ceil(
+      this.props.totalRecords / this.props.recordsPerPage
+    );
+    this.setState({
+      paginationEnd,
+      paginationCount: this.props.count,
+      selectedPage: this.props.selectedPage
+    });
   }
   componentWillReceiveProps(nextProps) {
+    const paginationEnd = Math.ceil(
+      nextProps.totalRecords / nextProps.recordsPerPage
+    );
     this.setState({
-      selectedPage: nextProps.selectedPage,
+      paginationEnd,
       paginationCount: nextProps.count,
-      paginationEnd: nextProps.totalPages
-    })
+      selectedPage: nextProps.selectedPage
+    });
   }
   handlePageChange(pageInd) {
-    this.setState({ selectedPage: pageInd })
+    this.setState({ selectedPage: pageInd });
     var divitionPoint = Math.floor(this.state.paginationCount / 2);
-    var pageStartPoint = pageInd - divitionPoint
+    var pageStartPoint = pageInd - divitionPoint;
     if (pageStartPoint < 1) {
       pageStartPoint = 1;
     }
-    if (pageStartPoint + this.state.paginationCount > this.state.paginationEnd + 1) {
-      pageStartPoint = Math.abs((this.state.paginationEnd + 1) - this.state.paginationCount)
+    if (
+      pageStartPoint + this.state.paginationCount >
+      this.state.paginationEnd + 1
+    ) {
+      pageStartPoint = Math.abs(
+        this.state.paginationEnd + 1 - this.state.paginationCount
+      );
     }
 
     if (this.state.paginationEnd > pageInd) {
-      this.setState({ paginationStart: pageStartPoint })
+      this.setState({ paginationStart: pageStartPoint });
     }
     // console.log("divitionPoint==", divitionPoint,
     // "-------pageStartPoint==", pageStartPoint, "=====pageInd=", pageInd, "--===****paginationEnd=", this.state.paginationEnd)
-    this.props.onPagination(pageInd)
+    this.props.onPagination(pageInd);
   }
 
   render() {
+    const { totalRecords, recordsPerPage } = this.props;
+    const remainderRecordsAtLastPage =
+      totalRecords % recordsPerPage || recordsPerPage;
+    const recordStartThisPage =
+      (this.state.selectedPage - 1) * recordsPerPage + 1;
+    const recordEndThisPage =
+      this.state.paginationEnd == this.state.selectedPage
+        ? recordStartThisPage + remainderRecordsAtLastPage - 1
+        : recordStartThisPage + recordsPerPage - 1;
+    console.log(
+      recordsPerPage,
+      remainderRecordsAtLastPage,
+      recordStartThisPage,
+      recordEndThisPage
+    );
     return (
       <div className="Pagination">
         {
-          <PaginationBody count={this.state.paginationCount} pageNo={this.state.selectedPage}
-            pageStart={this.state.paginationStart} pageEnd={this.state.paginationEnd}
-            onPageChange={this.handlePageChange} />
+          <PaginationBody
+            count={this.state.paginationCount}
+            pageNo={this.state.selectedPage}
+            pageStart={this.state.paginationStart}
+            pageEnd={this.state.paginationEnd}
+            onPageChange={this.handlePageChange}
+            recordStartThisPage={recordStartThisPage}
+            recordEndThisPage={recordEndThisPage}
+          />
         }
-
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-export default Pagination
+export default Pagination;
--- src/PaginationBody.js
+++ src/PaginationBody.js
@@ -1,32 +1,87 @@
-import React, { Component } from 'react';
-import times from 'lodash/times';
-import './index.css';
-import { EzFrame, EzCenter, EzInput, EzButton, EzLabelStyle } from './utility.js'
+import React, { Component } from "react";
+import times from "lodash/times";
+import "./index.css";
+import {
+  EzFrame,
+  EzCenter,
+  EzInput,
+  EzButton,
+  EzLabelStyle
+} from "./utility.js";
 
 class PaginationBody extends Component {
-
   render() {
     return (
       <div className="Pagination">
         <EzFrame big={true}>
           <EzCenter>
             <div className="pagination">
-              {this.props.pageNo > 1 ?
-                <span className="previous-btn" onClick={(evt) => this.props.onPageChange(this.props.pageNo - 1)}>Previous</span> : ''}
-              {times(this.props.count, (ind) =>
+              {
+                <span
+                  className="previous-btn"
+                  onClick={
+                    this.props.pageNo > 1
+                      ? evt => this.props.onPageChange(this.props.pageNo - 1)
+                      : () => {}
+                  }
+                  style={{ opacity: this.props.pageNo > 1 ? 0.7 : 0.5 }}
+                >
+                  Previous
+                </span>
+              }
+              {times(this.props.count, ind => (
                 // ind start from 0
-                <EzInput key={ind + 'i'} type="radio" name="nav" id={`input-${ind + this.props.pageStart}`} className="input"
+                <EzInput
+                  key={ind + "i"}
+                  type="radio"
+                  name="nav"
+                  id={`input-${ind + this.props.pageStart}`}
+                  className="input"
                   checked={this.props.pageNo === ind + this.props.pageStart}
-                  onChange={(evt) => this.props.onPageChange(ind + this.props.pageStart)} />
-              )}
-              {times(this.props.count, (ind) =>
+                  onChange={evt =>
+                    this.props.onPageChange(ind + this.props.pageStart)
+                  }
+                />
+              ))}
+              {times(this.props.count, ind => (
                 // ind start from 0
-                <EzButton key={ind + 'l'}
+                <EzButton
+                  key={ind + "l"}
                   htmlFor={`input-${ind + this.props.pageStart}`}
                   className={`button`}
-                  forChecked={this.props.pageNo === ind + this.props.pageStart} >{ind + this.props.pageStart}</EzButton>)}
-              {this.props.pageEnd > this.props.pageNo ?
-                <span className="next-btn" onClick={(evt) => this.props.onPageChange(this.props.pageNo + 1)}>Next</span> : ''}
+                  forChecked={this.props.pageNo === ind + this.props.pageStart}
+                >
+                  {ind + this.props.pageStart}
+                </EzButton>
+              ))}
+              {
+                <span
+                  className="next-btn"
+                  onClick={
+                    this.props.pageEnd > this.props.pageNo
+                      ? evt => this.props.onPageChange(this.props.pageNo + 1)
+                      : () => {}
+                  }
+                  style={{
+                    opacity: this.props.pageEnd > this.props.pageNo ? 0.7 : 0.5
+                  }}
+                >
+                  Next
+                </span>
+              }
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: 10,
+                fontSize: 12,
+                fontWeight: 600,
+                opacity: 0.7
+              }}
+            >
+              {" "}
+              [ Showing {this.props.recordStartThisPage} -{" "}
+              {this.props.recordEndThisPage} ]
             </div>
           </EzCenter>
         </EzFrame>
